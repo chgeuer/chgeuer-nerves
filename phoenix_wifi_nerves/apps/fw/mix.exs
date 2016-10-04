0df1f44479acd97be5829eac7967e91937c8141c
Christian Geuer-Pollmann <christian.geuer-pollmann@web.de>
2016-10-04 22:11:15 +0200

    a

--- phoenix_wifi_nerves/apps/fw/mix.exs
+++ phoenix_wifi_nerves/apps/fw/mix.exs
@@ -8,33 +8,44 @@ defmodule Fw.Mixfile do
      version: "0.0.1",
      target: @target,
      archives: [nerves_bootstrap: "~> 0.1.4"],
-     deps_path: "deps/#{@target}",
-     build_path: "_build/#{@target}",
+     deps_path: "../../deps/#{@target}",
+     build_path: "../../_build/#{@target}",
+     config_path: "../../config/config.exs",
+     lockfile: "../../mix.lock",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     aliases: aliases,
-     deps: deps ++ system(@target)]
+     aliases: aliases(Mix.env),
+     deps: deps ++ system(@target, Mix.env)]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
-    [mod: {Fw, []},
-     applications: [:logger]]
+     [mod: {Fw, []},
+      applications: applications(Mix.env)]
   end
+
+  defp applications(:prod), do: [:nerves_interim_wifi | general_apps]
+  defp applications(_), do: general_apps
+  defp general_apps, do: [:logger, :web]
 
   def deps do
-    [{:nerves, "~> 0.3.0"}]
+    [
+      {:nerves, "~> 0.3.0"},
+      {:nerves_interim_wifi, "~> 0.1.0", only: :prod},
+      {:web, in_umbrella: true},
+      {:dummy_nerves, in_umbrella: true, only: [:dev, :test]}
+   ]
   end
 
-  def system(target) do
-    [{:"nerves_system_#{target}", ">= 0.0.0"}]
+  def system(target, :prod) do
+    [
+      {:"nerves_system_#{target}", ">= 0.0.0"},
+    ]
   end
+  def system(_, _), do: []
 
-  def aliases do
-    ["deps.precompile": ["nerves.precompile", "deps.precompile"],
-     "deps.loadpaths":  ["deps.loadpaths", "nerves.loadpaths"]]
+  def aliases(:prod) do
+      ["deps.precompile": ["nerves.precompile", "deps.precompile"],
+       "deps.loadpaths":  ["deps.loadpaths", "nerves.loadpaths"]]
   end
-
+  def aliases(_), do: []
 end
