0a34e2782411d17bc4990c66916b827f9eb39d23
Christian Geuer-Pollmann <christian.geuer-pollmann@web.de>
2016-10-04 22:32:32 +0200

    a

--- phoenix_wifi_nerves/apps/fw/mix.exs
+++ phoenix_wifi_nerves/apps/fw/mix.exs
@@ -25,7 +25,7 @@ defmodule Fw.Mixfile do
 
   defp applications(:prod), do: [:nerves_interim_wifi | general_apps]
   defp applications(_), do: general_apps
-  defp general_apps, do: [:logger, :web]
+  defp general_apps, do: [:logger, :runtime_tools, :web]
 
   def deps do
     [
