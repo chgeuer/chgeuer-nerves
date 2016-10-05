5de62d4eb7e74774c4a63c5e49e6827fb68d827e
Christian Geuer-Pollmann <christian.geuer-pollmann@web.de>
2016-10-05 14:25:56 +0200

    a

--- phoenix_wifi_nerves/mix.exs
+++ phoenix_wifi_nerves/mix.exs
@@ -7,6 +7,7 @@ defmodule PhoenixWifiNerves.Mixfile do
     [apps_path: "apps",
      version: "0.1.0",
      elixir: "~> 1.3",
+     target: @target,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
