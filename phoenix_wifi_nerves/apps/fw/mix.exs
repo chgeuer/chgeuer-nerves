de772239c5e12393a9fa5dbc8233a2eb0d5fcef2
Christian Geuer-Pollmann <christian.geuer-pollmann@web.de>
2016-10-05 09:46:28 +0200

    a

--- phoenix_wifi_nerves/apps/fw/mix.exs
+++ phoenix_wifi_nerves/apps/fw/mix.exs
@@ -32,7 +32,7 @@ defmodule Fw.Mixfile do
       {:nerves, "~> 0.3.0"},
       {:web, in_umbrella: true},
       {:porcelain, ">= 0.0.0" },
-      {:nerves_interim_wifi, "~> 0.1.0", only: :prod},
+      {:nerves_interim_wifi, "~> 0.1.0", in_umbrella: true, only: :prod},
       {:dummy_nerves, in_umbrella: true, only: [:dev, :test]}
    ]
   end
